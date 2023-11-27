be603678a53959a13d09901c4c85e7691dfcdccf
62985 <62985@edu.campusvejle.dk>
2023-11-27 11:55:06 +0100

    B)

--- cook.py
+++ cook.py
@@ -1,2 +1,8 @@
-age = "pizza"
-print(age)
+def talk(phrase):
+    def say(word):
+        print(word)
+
+    words = phrase.str(' ')
+    for word in words:
+
+talk(str('I am going to buy the milk'))
