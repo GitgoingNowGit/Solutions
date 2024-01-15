bcac905488d6c46fe24c1b138afb66142d7cd017
62985 <62985@edu.campusvejle.dk>
2024-01-15 13:41:07 +0100

    B)

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-numbers = [8, 12, 3,"abc"]
-numbertext = "8, 12, 3, abc"
-for number in numbers:
-    print(f"{str(number).split("abc")}")
+class Animal:
+    def inspect(self):
+        print("Thats an animal :)")
+class Dog(Animal):
+    def dance(self):
+        print("That crazy dog is dancing")
+    def inspect_closely(self):
+        print("Thats a skinwalker (:")
+McDog = Dog()
 
-number_list = numbertext.split("")
-print(number_list)
-    # print(number)
-    # numbers = int(number)
-    # print(number>3)
+McDog.inspect()
+McDog.dance()
+McDog.inspect_closely()
