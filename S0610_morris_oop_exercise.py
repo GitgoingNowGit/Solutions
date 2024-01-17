3bb24e7c58fde3f110ba3b14fa83b5d38e82ed04
62985 <62985@edu.campusvejle.dk>
2024-01-17 09:34:40 +0100

    B)

---
+++ S0610_morris_oop_exercise.py
@@ -0,0 +1,44 @@
+"""
+Opgave "Morris The Miner" (denne gang objekt orienteret)
+
+Som altid skal du læse hele opgavebeskrivelsen omhyggeligt, før du begynder at løse opgaven.
+
+Kopier denne fil til din egen løsningsmappe. Skriv din løsning ind i kopien.
+
+Omskriv din oprindelige Morris-kode til en objektorienteret version.
+
+Definer en klasse Miner med attributter som sleepiness, thirst osv.
+og metoder som sleep, drink osv.
+Opret Morris og initialiser hans attributter ved at kalde konstruktoren for Miner:
+morris = Miner()
+
+Hvis du går i stå, så spørg google, de andre elever eller læreren (i denne rækkefølge).
+
+Når dit program er færdigt, skal du skubbe det til dit github-repository.
+Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
+Fortsæt derefter med den næste fil."""
+
+#globalize variables
+global gold
+global whiskey
+global sleepiness
+global thirst
+global hunger
+global turn
+global happiness
+
+def sleep():
+    sleepiness -= 25
+    happiness += 15
+def alive():
+    sleepiness += 5
+    happiness -= 10
+
+turn = 0
+hunger = 0
+thirst = 0
+sleepiness = 0
+whiskey = 0
+gold = 0
+happiness = 30
+
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+
 class Animal:
+
     def inspect(self):
         print("Thats an animal :)")
 class Dog(Animal):
+
     def dance(self):
         print("That crazy dog is dancing")
     def inspect_closely(self):
-        print("Thats a skinwalker (:")
+        print("Thats a skinwalker :,)")
+        global owner_sanity
+        owner_sanity += 51
+        print("You went insane, your sanity exceed 100. Your sanity:",owner_sanity)
+
+
 McDog = Dog()
+owner_sanity = 50
+
 
 McDog.inspect()
 McDog.dance()
 McDog.inspect_closely()
+
