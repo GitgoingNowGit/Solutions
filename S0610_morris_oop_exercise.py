8c12a1e2d7374c5e8f29caf3b6c5d550b92d2f43
62985 <62985@edu.campusvejle.dk>
2024-01-30 09:23:08 +0100

    B)

--- S0610_morris_oop_exercise.py
+++ S0610_morris_oop_exercise.py
@@ -12,20 +12,67 @@ og metoder som sleep, drink osv.
 Opret Morris og initialiser hans attributter ved at kalde konstruktoren for Miner:
 morris = Miner()
 
+sleep:      sleepiness-=10, thirst+=1,  hunger+=1,  whisky+=0, gold+=0
+mine:       sleepiness+=5,  thirst+=5,  hunger+=5,  whisky+=0, gold+=5
+eat:        sleepiness+=5,  thirst-=5,  hunger-=20, whisky+=0, gold-=2
+buy_whisky: sleepiness+=5,  thirst+=1,  hunger+=1,  whisky+=1, gold-=1
+drink:      sleepiness+=5,  thirst-=15, hunger-=1,  whisky-=1, gold+=0
+
 Hvis du går i stå, så spørg google, de andre elever eller læreren (i denne rækkefølge).
 
 Når dit program er færdigt, skal du skubbe det til dit github-repository.
 Send derefter denne Teams-meddelelse til din lærer: <filename> færdig
 Fortsæt derefter med den næste fil."""
+
+
 class Miner():
-    def __init__(self):
-        self.sleepiness
+
 
     def sleep(self):
-        print("Morris sleeps")
-        self.sleepiness = 1
+        morris.sleepiness -= 10
+        morris.thirst += 1
+        if morris.sleepiness < 0:
+            morris.sleepiness = 0
+        print("M. slept")
+
+    def mine(self):
+        morris.sleepiness += 5
+        morris.thirst += 5
+        print("M. mined")
+
+    def drink(self):
+        if morris.whisky > 0 and morris.thirst > 0:
+            morris.thirst -= 15
+            morris.sleepiness += 5
+        elif morris.thirst < 0:
+            morris.thirst = 0
+        print("M. drank")
+
+    def eat(self):
+        morris.hunger -= 20
+        morris.thirst += 5
+
+
+    def __init__(self, sleepiness=0, thirst=0, hunger=0, whisky=0, gold=0, turn=0):
+        self.sleepiness = sleepiness
+        self.thirst = thirst
+        self.hunger = hunger
+        self.whisky = whisky
+        self.gold = gold
+        self.turn = turn
+
+
+def dead():
+    return morris.sleepiness > 100 or morris.thirst > 100 or morris.hunger > 100
+
 
 morris = Miner()
-
-morris.sleep()
-print(sleepiness)
+#morris is very stupid and doesn't know he is a giant retarded pile of dumpster code, so he mines, killing himself slowly
+while not dead() and morris.turn < 1000:
+    morris.mine()
+    morris.sleep()
+    morris.drink()
+    morris.turn += 1
+    print("Morris sleepiness:", morris.sleepiness, "Morris thirst:", morris.thirst, "Morris hunger:", morris.hunger, "Turn:", morris.turn)
+else:
+    print("Morris died or the turn limit of 1000 was met.")
