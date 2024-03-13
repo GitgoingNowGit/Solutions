0adfe2865c6363cbb9ba824e22ef1fdbe793e0fb
62985 <62985@edu.campusvejle.dk>
2024-03-13 10:44:35 +0100

    B)

--- S1800_number_guessing.py
+++ S1800_number_guessing.py
@@ -22,19 +22,43 @@ import random
 
 start = 1000
 end = 9999
-global schwarz_coin
-global Weiss_coin
 
-print(f'This is a guessing game. Your task is to guess a four digit integer between {start} and {end}. \nYou will guess the integers individually till you have all four digits.')
-answer = random.randint(start,end)
-guess = input('\nEnter your guess for the first number here: ')
-if int(str(guess)) == int(str(answer)[:1]):
-    print("You guessed the correct number! +1 schwarz coin")
-else:
-    print(f'You guessed wrong, the correct number was {int(str(answer)[0:1])}')
+black = 0
+white = 0
+def calculate_coins(guess, answer):
+    global white
+    global black
+    for cifre in guess:
+        if cifre in answer:
+            white += 1
+    for guess_cifre, answer_cifre in zip(guess, answer):
+        if guess_cifre == answer_cifre:
+            black += 1
+    white -= black
+    return white, black
 
-guess = input(f'\nEnter your guess for the second number here: ')
-if int(str(guess)) == int(str(answer)[1:2]):
-    print("You guessed the correct number! +1 schwarz coin")
-else:
-    print(f'You guessed wrong, the correct number was {int(str(answer)[1:2])}')
+print(f'This is a guessing game. Your task is to guess a four digit integer between {start} and {end}.\n\nIf you guess the correct number, you will recieve a black coin.\nAnd if you guess a correct number, but in the wrong order you will recieve a weiss coin.\nYour score will be displayed at the end.\n')
+answer = str(random.randint(start,end))
+answer = '1234'
+attempts = 0
+while black <= 4:
+    guess = input(f'Enter your guess here: ')
+    white, black = calculate_coins(guess, answer)
+#if 4 black coins congratulate the player, end game
+#if between 0 and 3 black coins, print out number of black and white coins, and prompt player to guess again
+#do this until the player gets 4 black coins
+    if guess == answer:
+       print(f'You guessed the correct numbr! +1 schwarz coin ({black})')
+    elif guess in answer:
+     print(f'You guessed partially correct, right number but wrong order. +1 weiss coin ({white})')
+     attempts += 1
+    else:
+        attempts += 1
+print(f'\nCongratulations you fool.\nYou got a total of {black} black coins, and a total of {white} white coins.\nIt took you {attempts} attempts to guess the correct number.')
+print('Now, please stop gambling your lifesavings, this isnt one of "those" casinos.')
+
+# guess = input(f'\nEnter your guess for the second digit here: ')
+# if int(str(guess)) == int(str(answer)[1:2]):
+#     print("You guessed the correct number! +1 schwarz coin")
+# else:
+#     print(f'You guessed wrong, the correct number was {int(str(answer)[1:2])}')
